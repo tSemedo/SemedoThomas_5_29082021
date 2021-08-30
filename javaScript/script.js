13acc89521a460498db140bc51d486006343efb1
Semedo <semedothomas@hotmail.fr>
2021-08-30 15:39:01 +0200

    Ajoute premiers ligne de JavaScript

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,20 +1,32 @@
-const img = document.getElementById ('img0')
+let img = document.getElementById('img0')
+let name = document.getElementById('name0')
+let description = document.getElementById('description0')
+let price = document.getElementById('price0')
+let idd = document.querySelector('link0')
+// let i = [];
 
-const name = document.getElementById ('name0')
 
-const description = document.getElementById ('description0')
-
-const price = document.getElementById ('price0')
+// function affichageProduits(data){
+//     const positionElements = document.querySelector('.container-produits')
+    
+//     data.forEach(element,i) => {
+//         name[i] = element.name
+//     });
+    
+//     console.log(positionElements)
+// }
+// affichageProduits();
+// console.log('affichageProduits')
 
 fetch ('http://localhost:3000/api/teddies')
-    .then ( res=> res.json())
-    .then (data=> { 
-        name.textContent = data[0].name ;
-        img.src = data[0].imageUrl;
-        description.textContent = data[0].description
-        price.textContent = data[0].price
-    });
+    .then (res=> res.json())
+    .then (data=> {          
+        name0.textContent = data[0].name ;
+        img0.src = data[0].imageUrl;
+        description0.textContent = data[0].description
+        price0.textContent = data[0].price
 
+      }) 
 
 fetch ('http://localhost:3000/api/teddies')
     .then ( res=> res.json())
@@ -53,7 +65,13 @@ fetch ('http://localhost:3000/api/teddies')
     })
 
 
-fetch ('http://localhost:3000/api/teddies')
-    .then (res=> res.json())
-    .then (data => console.log(data))
+// fetch ('http://localhost:3000/api/teddies')
+//     .then (res=> res.json())
+//     .then (data => console.log(data))
 
+    // { 
+    //     name.textContent = data[0].name ;
+    //     img.src = data[0].imageUrl;
+    //     description.textContent = data[0].description
+    //     price.textContent = data[0].price               
+    // })
